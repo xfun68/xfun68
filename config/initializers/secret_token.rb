1af1c22bc0ebf39fb38c0bc530a9843542673ca8
xfun68 <xfun68@gmail.com>
2010-09-23 16:15:01 +0800

    cleare files

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Xfun68::Application.config.secret_token = 'adea3e691cbed91b90f6bca0cad92efd9617c2038a9e146e62ef9cebbcef709936eba1eea79f0b78c915ac2de2bbe1458c086002c6061570979d5d2faccba806'
--- config/initializers/session_store.rb
+++
@@ -1,8 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-Xfun68::Application.config.session_store :cookie_store, :key => '_xfun68_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rake db:sessions:create")
-# Xfun68::Application.config.session_store :active_record_store
